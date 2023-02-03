511b5cb3bcb03526ffbae74beab0fc7941b341f0
Nina Yularzhi <ninkayularji@mail.ru>
2023-02-03 17:27:46 +0300

    dz

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿string [] arr = {"hello", "yes", "no", "4", "world", "ok", "5", "task" };
+
+int x = 0;
+
+for( int i = 0; i< arr.Length; i++)
+{
+    if( arr[i].Length<4) 
+    x++;
+}    
+string [] arr2 = new string [x];
+    
+void FillArray()
+{
+    int j = 0;
+    for( int i = 0; i< arr.Length; i++)
+    {
+        if ( arr[i].Length < 4)
+        {
+            arr2[j] = arr[i];
+            j++;
+        }
+    }
+}
+
+
+void PrintArray()
+{
+    for (int i = 0; i < arr2.Length; i++)
+    {
+        if (i < arr2.Length - 1) 
+        Console.Write($"{arr2[i]}  ");
+        else 
+        Console.Write(arr2[i]);
+    }
+    Console.WriteLine();
+}
+
+FillArray();
+PrintArray();
+
+
